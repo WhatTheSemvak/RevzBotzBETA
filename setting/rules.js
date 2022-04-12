83dbba91cd05a4afa83d2575d46d2ac350869c38
WhatTheSemvak <ivanedrayasa90@gmail.com>
2022-04-12 07:28:19 +0700

    RevzBotz ID

---
+++ setting/rules.js
@@ -0,0 +1,25 @@
+/*Kalian bisa ganti ini rulesnya
+ingat !! yang ${botname} jangan di ganti
+biarin aja sans ntar bakalan keluar nama bot luh kok
+*/
+const rules = (prefix, botname) => { 
+return `*_RULES BOT_*
+
+1. Jangan spam bot. 
+Sanksi: *WARN/KICK APABILA DI GRUP*
+
+2. Jangan telepon bot.
+Sanksi: *AUTO BLOCK*
+
+3. Sabar Ketika Bot Pending Dll.
+Sanksi: *Kena Roastinx Oleh Owner*
+
+4.Pahami Dulu Cara Menggunakan Bot! Ga Ngerti? Tanya Owner
+
+5.Jangan Ngeluh Mulu Ngentot Udah Gratisan Pengen Instan
+
+Jika sudah dipahami rules-nya, silakan ketik *${prefix}menu* untuk memulai!
+
+──「 *${botname}* 」──`
+}
+exports.rules = rules
